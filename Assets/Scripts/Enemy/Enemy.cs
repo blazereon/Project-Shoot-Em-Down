691e3972b6d0d373f07820dff91beb4fe42f216c
jp-jp-123 <jpbeltran098@gmail.com>
2025-04-16 13:15:53 +0800

    Universal Direction
    Accomodates either enum or translate for direction

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -20,6 +20,12 @@ public class Enemy : MonoBehaviour
     public bool isPlayerDetected;
     [HideInInspector]
     public bool[] hitRays;
+    [HideInInspector]
+    public enum EnemyFacing
+    {
+        Left,
+        Right
+    }
 
     private void Start()
     {
@@ -49,6 +55,46 @@ public class Enemy : MonoBehaviour
         for (int i = 0; i < rayNumber; i++)
         {
             Vector3 _rayDirection = Quaternion.Euler(0, 0, _currentAngle + (i * (rayMaxAngle / rayNumber))) * transform.right * transform.localScale.x;
+
+            // Cast a raycast only if no players are detected, could be more optimized this way
+            if (!_isPlayerDetected)
+            {
+                RaycastHit2D _hit = Physics2D.Raycast(transform.position, _rayDirection, detectionRange);
+                Debug.DrawRay(transform.position, _rayDirection * detectionRange, Color.green);
+
+                if (_hit)
+                {
+                    // code
+                    if (_hit.collider.name == "Player")
+                    {
+
+                        Debug.DrawRay(transform.position, _rayDirection * detectionRange, Color.yellow);
+
+                        _isPlayerDetected = true;
+                        Debug.Log("HIT!");
+                    }
+                }
+                else
+                {
+                    _isPlayerDetected = false;
+                }
+            }
+        }
+
+        return _isPlayerDetected;
+    }
+
+    public bool PlayerDetection(EnemyFacing _faceDirection)
+    {
+        bool _isPlayerDetected = false;
+
+        float _currentAngle = rayStartAngle;
+
+        Debug.Log("SCANNING");
+
+        for (int i = 0; i < rayNumber; i++)
+        {
+            Vector3 _rayDirection = Quaternion.Euler(0, 0, _currentAngle + (i * (rayMaxAngle / rayNumber))) * transform.right * (_faceDirection == EnemyFacing.Left ? -1 : 1);
 
             // Cast a raycast only if no players are detected, could be more optimized this way
             if (!_isPlayerDetected)
