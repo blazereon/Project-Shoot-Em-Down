93e07d49e31c7702a5bb1c7877cbac8111c2fbc8
recoder_99 <roelcastro_99@outlook.ph>
2025-04-16 12:39:37 +0800

    added wandering state

--- Assets/Scripts/Enemy/Melee/GenericEnemyManager.cs
+++ Assets/Scripts/Enemy/Melee/GenericEnemyManager.cs
@@ -1,13 +1,33 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+
+public enum EnemyFacing
+{
+    Left,
+    Right
+}
 
 public class GenericEnemyManager : Enemy
 {
 
     GenericEnemyBaseState currentState;
 
-    GenericEnemyWanderState wanderState = new GenericEnemyWanderState();
-    GenericEnemyChaseState chaseState = new GenericEnemyChaseState();
-    GenericEnemyAttackState attackState = new GenericEnemyAttackState();
+    public GenericEnemyWanderState wanderState = new GenericEnemyWanderState();
+    public GenericEnemyChaseState chaseState = new GenericEnemyChaseState();
+    public GenericEnemyAttackState attackState = new GenericEnemyAttackState();
+
+
+    public EnemyFacing facing = EnemyFacing.Left;
+    public float WanderSpeed;
+    public float ChasingSpeed;
+
+
+    //Generic Enemy Variables
+
+    public Rigidbody2D GenericEnemyRb;
+
+    public float WallDistanceLimit;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,5 +50,31 @@ public class GenericEnemyManager : Enemy
     public void SwitchState(GenericEnemyBaseState state)
     {
         currentState = state;
+        currentState.EnterState(this);
+    }
+
+    public void Flip()
+    {
+        facing = facing == EnemyFacing.Left ? EnemyFacing.Right : EnemyFacing.Left;
+        UpdateFlip();
+    }
+
+    public void Flip(EnemyFacing face)
+    {
+        facing = face;
+        UpdateFlip();
+    }
+
+    void UpdateFlip()
+    {
+        if (facing == EnemyFacing.Left)
+
+        {
+
+        }
+        else if (facing == EnemyFacing.Right)
+        {
+
+        }
     }
 }
--- Assets/Scripts/Enemy/Melee/GenericEnemyWanderState.cs
+++ Assets/Scripts/Enemy/Melee/GenericEnemyWanderState.cs
@@ -1,17 +1,62 @@
+using UnityEngine;
+
 public class GenericEnemyWanderState : GenericEnemyBaseState
 {
+    private float _detectRangeInstance;
+    private LayerMask _layerMask;
+
     public override void EnterState(GenericEnemyManager genericEnemy)
     {
-        
+        _detectRangeInstance = genericEnemy.detectionRange;
+        _layerMask = LayerMask.GetMask("Wall", "Player");
     }
 
     public override void UpdateState(GenericEnemyManager genericEnemy)
     {
-        
+        bool _isPlayerDetected = genericEnemy.PlayerDetection();
+
+        if (_isPlayerDetected)
+        {
+            genericEnemy.SwitchState(genericEnemy.chaseState);
+        }
+
+        Debug.Log(genericEnemy.facing);
+
+
+        //wall logic
+        if (genericEnemy.facing == EnemyFacing.Left)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(genericEnemy.transform.position, Vector2.left, genericEnemy.WallDistanceLimit);
+            Debug.DrawRay(genericEnemy.transform.position, Vector2.left * genericEnemy.WallDistanceLimit, Color.cyan );
+            if (hit.collider == null) return;
+            if (hit.collider.tag == "Wall")
+            {
+                Debug.LogWarning("Wall hit");
+                genericEnemy.Flip(EnemyFacing.Right);
+            }
+        }
+        else if (genericEnemy.facing == EnemyFacing.Right)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(genericEnemy.transform.position, Vector2.right, genericEnemy.WallDistanceLimit);
+            Debug.DrawRay(genericEnemy.transform.position, Vector2.right * genericEnemy.WallDistanceLimit, Color.cyan );
+            if (hit.collider == null) return;
+            if (hit.collider.tag == "Wall")
+            {
+                Debug.LogWarning("Wall hit");
+                genericEnemy.Flip(EnemyFacing.Left);
+            }
+        }
     }
 
     public override void FixedUpdateState(GenericEnemyManager genericEnemy)
     {
-        
+        if (genericEnemy.facing == EnemyFacing.Left)
+        {
+            genericEnemy.GenericEnemyRb.linearVelocityX = Vector2.left.x * genericEnemy.WanderSpeed * Time.fixedDeltaTime;
+        }
+        else if (genericEnemy.facing == EnemyFacing.Right)
+        {
+            genericEnemy.GenericEnemyRb.linearVelocityX = Vector2.right.x * genericEnemy.WanderSpeed * Time.fixedDeltaTime;
+        }
     }
 }
