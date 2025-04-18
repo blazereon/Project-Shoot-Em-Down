114de6abc15874364e56b8ca06eb3edcdb3c9c66
jp-jp-123 <jpbeltran098@gmail.com>
2025-04-18 19:06:04 +0800

    Basic enemy states for ranged grounded

---
+++ Assets/Scripts/Enemy/Ranged/GenericRangedGround/AttackRangedGrounded.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AttackRangedGrounded : BaseRangedGrounded
+{
+    public override void EnterState(ManagerRangedGrounded enemy)
+    {
+        
+    }
+
+    public override void UpdateState(ManagerRangedGrounded enemy)
+    {
+        
+    }
+
+    public override void FixedUpdateState(ManagerRangedGrounded enemy)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Enemy/Ranged/GenericRangedGround/BaseRangedGrounded.cs
@@ -0,0 +1,6 @@
+public abstract class BaseRangedGrounded
+{
+    public abstract void EnterState(ManagerRangedGrounded enemy);
+    public abstract void UpdateState(ManagerRangedGrounded enemy);
+    public abstract void FixedUpdateState(ManagerRangedGrounded enemy);
+}
