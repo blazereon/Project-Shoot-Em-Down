5847a206ad5e29006465d1ff6890060eb614187d
recoder_99 <roelcastro_99@outlook.ph>
2025-04-27 01:01:32 +0800

    attack state now working
    note: attack state only works from idle state for now

--- Assets/Scripts/Player/PlayerState/AttackPlayerState.cs
+++ Assets/Scripts/Player/PlayerState/AttackPlayerState.cs
@@ -1,28 +1,62 @@
+using UnityEditor;
 using UnityEngine;
 
 public class AttackPlayerState : BasePlayerState
 {
+    private float _meleeRange = 1.5f;
+    private LayerMask _layerMask;
     public override void EnterState(ManagerPlayerState player)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Attack Invoked");
+        _layerMask = LayerMask.GetMask("Wall", "Enemy");
+        RaycastHit2D[] hits;
+
+        if (player.facing == Facing.right)
+        {
+            hits = Physics2D.RaycastAll(player.transform.position, Vector2.right, _meleeRange, _layerMask);
+            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
+            DamageEnemy(hits);
+            player.PopState();
+            return;
+        }
+        else if (player.facing == Facing.left)
+        {
+            hits = Physics2D.RaycastAll(player.transform.position, Vector2.left, _meleeRange, _layerMask);
+            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
+            DamageEnemy(hits);
+            player.PopState();
+            return;
+        }
     }
 
     public override void UpdateState(ManagerPlayerState player)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void FixedUpdateState(ManagerPlayerState player)
     {
-        throw new System.NotImplementedException();
+        
     }
     public override void OnCollisionEnter2DState(Collision2D collision, ManagerPlayerState player)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void OnCollisionExit2DState(Collision2D collision, ManagerPlayerState player)
     {
-        throw new System.NotImplementedException();
+        
+    }
+
+    private void DamageEnemy(RaycastHit2D[] hits)
+    {
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider == null) continue;
+            if (hit.transform.tag == "Enemy")
+            {
+                EventSystem.Current.AttackEnemy(hit.collider.gameObject, 15);
+            }
+        }
     }
 }
