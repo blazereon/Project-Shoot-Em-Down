14af4b1b4a26fa8b79fb5cb3cbaf87da8d80ab36
recoder_99 <roelcastro_99@outlook.ph>
2025-05-11 18:23:57 +0800

    attack_bak

--- Assets/Scripts/Player/PlayerState/AttackPlayerState.cs
+++ Assets/Scripts/Player/PlayerState/AttackPlayerState.cs
@@ -5,31 +5,23 @@ public class AttackPlayerState : BasePlayerState
 {
     private float _meleeRange = 1.5f;
     private LayerMask _layerMask;
-
     private ManagerPlayerState _player;
     public override void EnterState(ManagerPlayerState player)
     {
-        _player = player;   
-        Debug.Log("Attack Invoked");
-        _layerMask = LayerMask.GetMask("Wall", "Enemy");
-        RaycastHit2D[] hits;
+        switch (player.attackType)
+        {
+            case Player.AttackType.Melee:
+                MeleeAttack(player);
+                break;
+            case Player.AttackType.Ranged:
+                RangeAttack(player);
+                break;
+            default:
+                Debug.LogError("Attack type not specified");
+                break;
+        }
+        player.PopState();
 
-        if (player.facing == Facing.right)
-        {
-            hits = Physics2D.RaycastAll(player.transform.position, Vector2.right, _meleeRange, _layerMask);
-            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
-            DamageEnemy(hits);
-            player.PopState();
-            return;
-        }
-        else if (player.facing == Facing.left)
-        {
-            hits = Physics2D.RaycastAll(player.transform.position, Vector2.left, _meleeRange, _layerMask);
-            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
-            DamageEnemy(hits);
-            player.PopState();
-            return;
-        }
     }
 
     public override void UpdateState(ManagerPlayerState player)
@@ -73,5 +65,35 @@ public class AttackPlayerState : BasePlayerState
                 //Modify projectile trajectory and shit
             }
         }
+    }
+
+    private void MeleeAttack(ManagerPlayerState player)
+    {
+        _player = player;
+        Debug.Log("Attack Invoked");
+        _layerMask = LayerMask.GetMask("Wall", "Enemy");
+        RaycastHit2D[] hits;
+
+        if (player.facing == Facing.right)
+        {
+            hits = Physics2D.RaycastAll(player.transform.position, Vector2.right, _meleeRange, _layerMask);
+            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
+            DamageEnemy(hits);
+            player.PopState();
+            return;
+        }
+        else if (player.facing == Facing.left)
+        {
+            hits = Physics2D.RaycastAll(player.transform.position, Vector2.left, _meleeRange, _layerMask);
+            Debug.DrawRay(player.transform.position, Vector2.right * _meleeRange, Color.cyan, 0.2f);
+            DamageEnemy(hits);
+            player.PopState();
+            return;
+        }
+    }
+
+    private void RangeAttack(ManagerPlayerState player)
+    {
+
     }
 }
