5a3009e82b8d57c8432a10888754dc7a1e352761
jp-jp-123 <jpbeltran098@gmail.com>
2025-06-01 19:37:43 +0800

    Piercing projectile property

--- Assets/Scripts/Enemy/Ranged/Projectile.cs
+++ Assets/Scripts/Enemy/Ranged/Projectile.cs
@@ -54,7 +54,12 @@ public class Projectile : MonoBehaviour
         {
             Debug.Log("Player hit!");
             EventSystem.Current.AttackPlayer(ProjectileCurrentProperties.AttackDamage);
-            Destroy(gameObject);
+
+            if (!ProjectileCurrentProperties.isPiercing)
+            {
+                Destroy(gameObject);
+            }
+            
         }
         else if ((collision.gameObject.layer == _enemyLayer || collision.gameObject.layer == _nonCollideEnemy) && ProjectileCurrentProperties.FiredBy == ProjectileOwner.Player)
         {
@@ -70,8 +75,11 @@ public class Projectile : MonoBehaviour
                 Debug.Log("Enemy hit! On Non weak spot");
                 EventSystem.Current.AttackEnemy(collision.gameObject, DamageType.Range, ProjectileCurrentProperties.AttackDamage, 0, false);
             }
-                
-            Destroy(gameObject);
+
+            if (!ProjectileCurrentProperties.isPiercing)
+            {
+                Destroy(gameObject);
+            }
         }
         else
         {
