b795a01c07d52b31b8a401457b1a5cfff65812a5
recoder99 <30973326+recoder99@users.noreply.github.com>
2025-07-15 12:37:14 +0800

    Ability Tree now controls tiers

--- Assets/Scripts/UI/AbilityTree.cs
+++ Assets/Scripts/UI/AbilityTree.cs
@@ -1,16 +1,69 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbilityTree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public String AbilityName;
+    public Sprite Tier1Icon, Tier2Icon, Tier3Icon;
+    public Sprite FrameSprite, LineSprite;
+    public Tree SkillTree;
+    public TextMeshProUGUI AbilityNameTMP;
+
+    void OnValidate()
     {
-        
+        InitializeTree();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        InitializeTree();
     }
+
+    void InitializeTree()
+    {
+        //Ability Name
+        if (AbilityName != null || AbilityName != "")
+        {
+            AbilityNameTMP.text = AbilityName;
+        }
+        //Icon
+        if (Tier1Icon != null)
+        {
+            SkillTree.AbilityTier[0].IconSprite = Tier1Icon;
+        }
+        if (Tier2Icon != null)
+        {
+            SkillTree.AbilityTier[1].IconSprite = Tier2Icon;
+        }
+        if (Tier3Icon != null)
+        {
+            SkillTree.AbilityTier[2].IconSprite = Tier3Icon;
+        }
+
+        //Frame Sprite
+
+        if (FrameSprite != null)
+        {
+            foreach (Tier currentTier in SkillTree.AbilityTier)
+            {
+                currentTier.FrameSprite = FrameSprite;
+            }
+
+            var MainFrame = this.GetComponent<Image>();
+
+            MainFrame.sprite = FrameSprite;
+        }
+
+        //Line Sprite
+        if (LineSprite != null)
+        {
+            foreach (Tier currentTier in SkillTree.AbilityTier)
+            {
+                currentTier.LineSprite = LineSprite;
+            }
+        }
+    }
+
 }
