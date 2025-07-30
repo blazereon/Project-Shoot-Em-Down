04976ec551650ee64ec9c503dc0d46f336f7662c
recoder99 <30973326+recoder99@users.noreply.github.com>
2025-07-30 16:04:30 +0800

    skill point tutorial handler

--- Assets/Scripts/Tutorial/TutorialHandler.cs
+++ Assets/Scripts/Tutorial/TutorialHandler.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class TutorialHandler : MonoBehaviour
 {
+    bool isSkillUpgradeInit = false;
+    void Awake()
+    {
+        EventSystem.Current.OnUpdatePlayerStats += OnPlayerStatUpdate;
+    }
 
+    private void OnPlayerStatUpdate(PlayerStats stats)
+    {
+        if (stats.SkillPoint >= 1 && !isSkillUpgradeInit)
+        {
+            // Show tutorial for skill point
+            Toast.ToastMessage msg = new Toast.ToastMessage
+            {
+                Title = "Skill Upgrade",
+                Message = "Upon Receiving a Skill Point, You can upgrade your skills by pressing [Tab].",
+                DisplayTime = 4f,
+            };
+            UIManager.Current.PushToast(msg);
+            isSkillUpgradeInit = true;
+        }
+    }
 }
