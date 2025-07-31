e4a1fce03790aa7b8eaf3ca11666fb782944e0b7
recoder99 <30973326+recoder99@users.noreply.github.com>
2025-07-31 16:42:27 +0800

    skill tree toast handler

--- Assets/Scripts/Tutorial/TutorialHandler.cs
+++ Assets/Scripts/Tutorial/TutorialHandler.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TutorialHandler : MonoBehaviour
 {
     bool isSkillUpgradeInit = false;
+    bool isSkillTreeExecute = false;
     void Awake()
     {
         EventSystem.Current.OnUpdatePlayerStats += OnPlayerStatUpdate;
@@ -21,6 +23,33 @@ public class TutorialHandler : MonoBehaviour
             };
             UIManager.Current.PushToast(msg);
             isSkillUpgradeInit = true;
+        }
+    }
+
+    void Update()
+    {
+        if (isSkillUpgradeInit && !isSkillTreeExecute)
+        {
+            if (UIManager.Current.CurrentState == UIState.SkillTree)
+            {
+                // Show tutorial for skill tree
+                var msg = new Toast.ToastMessage
+                {
+                    Title = "Skill Tree",
+                    Message = "Hover over skill to see its functionalities. Each skill consumes 1 skill point",
+                    DisplayTime = 3f,
+                };
+                UIManager.Current.PushToast(msg);
+
+                var msg2 = new Toast.ToastMessage
+                {
+                    Title = "Skill Tree",
+                    Message = "Left click and hold the selected skill until the whole skill turns into a color",
+                    DisplayTime = 3f,
+                };
+                UIManager.Current.PushToast(msg2);
+                isSkillTreeExecute = true;
+            }
         }
     }
 }
