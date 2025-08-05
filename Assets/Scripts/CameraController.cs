89831bedbb64d5637011cb58eac97f7979dbc637
recoder99 <30973326+recoder99@users.noreply.github.com>
2025-08-05 17:31:28 +0800

    More features added
    + Checkpoint System script (handles events)
    + Normal Checkpoints
    + Spawn Checkpoints
    + Singleton Camera Controller for Cinemachine
    + Void

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    CinemachineCamera CinemachineCameraInstance;
+    public static CameraController Current;
+    public CinemachineCamera CinemachineCameraInstance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-
-    private void TrackPlayer(GameObject player)
+    void Awake()
     {
-        
+        if (Current == null)
+        {
+            Current = this;
+        }
     }
-    void Update()
+    public void TrackTarget(GameObject player)
     {
-        
+        CinemachineCameraInstance.Follow = player.transform;
     }
 }
